06ab741fd9a047220c5f0fb97fed0654c99f03c5
Cecilia Nakamura <ceci.n.nakamura@gmail.com>
2024-05-13 18:19:31 -0300

    test setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import find_packages, setup
+import os
 
 entry_point = "spaceflight310 = spaceflight310.main:main"
 
 with open("requirements.txt") as f:
     required_packages = f.read().splitlines()
+
+# Include all files in the src, data, and conf directories
+data_files = []
+for directory in ["src", "data", "conf"]:
+    for dirname, _, filenames in os.walk(directory):
+        for filename in filenames:
+            data_files.append((os.path.join("spaceflight310", dirname), [os.path.join(dirname, filename)]))
 
 setup(
     name="spaceflight310",
@@ -11,5 +19,5 @@ setup(
     packages=find_packages(),
     install_requires=required_packages,
     entry_points={"console_scripts": [entry_point]},
-    include_package_data=True,  # Include data files specified in MANIFEST.in
+    data_files=data_files,  # Include data files
 )
